95fb51d67da7c20853bd947d13d0977cba3957c8
Stephen_ZQ <zhongqi1112@gmail.com>
2019-11-18 21:36:32 -0800

    Update file

--- source/firstRun.bat
+++ source/firstRun.bat
@@ -3,6 +3,8 @@ python -m venv %VENV%
 set PYTHONP=%VENV%\Scripts\python.exe
 set PIPP=%VENV%\Scripts\pip.exe
 set PSERVE=%VENV%\Scripts\pserve.exe
-%PYTHONP% %PIPP% install -e .
+%PYTHONP% %PIPP% install "pyramid==1.10.4"
+%PIPP% install pyramid_chameleon
+%PYTHONP% %PIPP% install -e ".[dev]"
 %PYTHONP% %PSERVE% development.ini --reload
 set /p DUMMY=Hit ENTER to continue ...
